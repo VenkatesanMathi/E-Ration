c89556265f06485eeeb881d426366048abb9fb60
Venkat <venkatesan.mathi06@gmail.com>
2024-08-18 19:24:22 +0530

    Adding Maven Spring details

---
+++ src/main/java/com/er/eration/ErationApplication.java
@@ -0,0 +1,13 @@
+package com.er.eration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/er/eration/ErationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.er.eration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
